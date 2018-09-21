613490aed71f2cd6c0e5eb53c8d4604d263da0be
Fred <sohnf90@yahoo.com>
2018-09-21 10:15:58 -0700

    Did register, added get users and get root

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,61 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
 
+const db = require('../database/dbConfig.js')
 const { authenticate } = require('./middlewares');
 
 module.exports = server => {
   server.post('/api/register', register);
   server.post('/api/login', login);
   server.get('/api/jokes', authenticate, getJokes);
+  server.get('/', serverCheck);
+  server.get('/api/users', getUsers);
 };
+
+function generateToken(user) {
+  const payload = {
+    username: user.username
+  };
+  const options = {
+    expiresIn: '1h',
+    jwtid: '2468'
+  };
+  return(token = jwt.sign(payload, secret, options));
+}
+
+function serverCheck(req, res) {
+  res.send('Server is running.');
+}
+
+function getUsers(req, res) {
+  db('users')
+    .select('id', 'username', 'password')
+    .then(users => {
+      res.status(200).json(users)
+    })
+    .catch(err => res.status(500).json({errorMsg: 'Could not get users.'}))
+}
 
 function register(req, res) {
   // implement user registration
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 16);
+  creds.password = hash;
+
+  db('users')
+    .insert(creds)
+    .then(ids => {
+      const id = ids[0];
+      // find user using id
+      db('users')
+        .where({id})
+        .first()
+        .then(user => {
+          const token = generateToken(user);
+          res.status(201).json({id: user.id, token})
+        })
+        .catch(err => res.status(500).send(err));
+    })
 }
 
 function login(req, res) {
